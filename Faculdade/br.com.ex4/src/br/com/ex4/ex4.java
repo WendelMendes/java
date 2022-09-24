a1f4d066d0b71a04e2528665a1aa4e40f8737337
Wendel Mendes <wendel.omendes@gmail.com>
2022-09-24 08:31:13 -0300

    Update ex4.java

--- Faculdade/br.com.ex4/src/br/com/ex4/ex4.java
+++ Faculdade/br.com.ex4/src/br/com/ex4/ex4.java
@@ -4,17 +4,6 @@ import java.util.Scanner;
 
 public class ex4 {
 	public static void main(String[] args) {
-		for (int i = 0; i != 2; i++) {
-			Scanner xx = new Scanner(System.in);
-			System.out.println("Digite seu nome: ");
-			String nome = xx.next();
-			System.out.println("Digite seu salário: ");
-			double salario = xx.nextDouble();
-			double somaSalario = 0;
-			somaSalario += salario;
-			System.out.println("Deseja continuar? 0-Sim | 1-Não :  ");
-			i = xx.nextInt();
-			xx.close();
-		}
+
 	}
 }
